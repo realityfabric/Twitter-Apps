303df2c7e3a618aa179b13e8834f8e513198f70c
realityfabric <realityfabric@gmail.com>
2018-01-26 13:22:04 -0800

    Add JavaDoc comments

--- TwitterHistoryCleanup.java
+++ TwitterHistoryCleanup.java
@@ -6,12 +6,26 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
+/**
+ *
+ * @author realityfabric
+ */
 public class TwitterHistoryCleanup {
 
+    /**
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
 
     }
 
+    /**
+     *
+     * @param twitter an initialized and authenticated Twitter object
+     * @return A List of all of the user's tweets.
+     * @throws TwitterException
+     */
     public static List<Status> getAllTweets (Twitter twitter) throws TwitterException {
         // this list will hold all of the users tweets that can be gathered
         List<Status> tweets;
