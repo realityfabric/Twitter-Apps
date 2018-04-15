f1e5b0541ff0eb9b162bef60a84e4da45a505cd5
realityfabric <realityfabric@gmail.com>
2018-04-15 10:43:00 -0700

    Create likeThread method

--- twitterapps/TwitterInteractions.java
+++ twitterapps/TwitterInteractions.java
@@ -7,20 +7,31 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
-public class TwitterInteractions {	
+public class TwitterInteractions {
 	public static ArrayList<Status> getThread (Twitter twitter, long endId, long startId)
 			throws TwitterException {
 		ArrayList<Status> thread = new ArrayList<>();
 		Status s = twitter.showStatus(endId);
-		
+
 		thread.add(s);
-		
+
 		do {
 			s = twitter.showStatus(s.getInReplyToStatusId());
 			thread.add(s);
 		} while (s.getId() != startId && s.getInReplyToStatusId() != -1);
-		
+
 		Collections.reverse(thread);
+		return thread;
+	}
+
+	public static ArrayList<Status> likeThread (Twitter twitter, long endId, long startId)
+			throws TwitterException {
+		ArrayList<Status> thread = getThread(twitter, endId, startId);
+
+		for (Status s : thread) {
+			twitter.createFavorite(s.getId());
+		}
+
 		return thread;
 	}
 }
